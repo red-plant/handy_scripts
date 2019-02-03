cb0eaf374a90a3b04babee4a202153dc8b7c7b8e
José Alaniz <jaime.alaniz.fabian@gmail.com>
2019-02-03 13:31:57 -0600

    Update multiline.to.single.line.fasta.stdinout.rb

--- multiline.to.single.line.fasta.stdinout.rb
+++ multiline.to.single.line.fasta.stdinout.rb
@@ -1,17 +1,24 @@
 #!/usr/bin/ruby
-i=0
-str=""
-arg = $stdin.readlines
-for i in 0...(arg.size)
 
-   if arg[i][0,1] != ">" 
-     str << arg[i].delete("\n")
+# Usage: cat multiline.fa | ./multiline.to.single.line.fasta.stdinout.rb > single.line.fa
+
+# Takes STDIN, removes line breaks in all lines not starting with the ">" character.
+# Outputs to STDOUT
+
+i=0
+sl=""
+ml = $stdin.readlines
+
+for i in 0...(ml.size)
+
+   if ml[i][0,1] != ">" 
+     sl << ml[i].delete("\n")
    end
    
-   if arg[i][0,1] == ">"
-    str << "\n"
-    str << arg[i]
+   if ml[i][0,1] == ">"
+    sl << "\n"
+    sl << ml[i]
    end
    
 end
-$stdout.puts(str[1,(str.size)-1])
+$stdout.puts(sl)

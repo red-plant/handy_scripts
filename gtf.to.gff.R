6d4e69b045f19bcd5f4aed76fa4317648179051c
José Alaniz <jaime.alaniz.fabian@gmail.com>
2019-03-01 18:15:38 -0600

    Create gtf.to.gff.R

---
+++ gtf.to.gff.R
@@ -0,0 +1,23 @@
+#only tested for araport11, with some editing it should work for any annotation. requires data.table package
+gff.to.gtf <- function(gff, write.to.file=F, file.path=NULL, gene.tag="ID=", transcript.tag="Parent="){
+  require(data.table)
+  gff <- data.table(gff)
+  if(ncol(gff)!=9){
+    stop("gff does not contain 9 columns, function expect a gff table: chromosome, db, type, start, end, score, strand, frame, description")
+  }
+  gtf <- gff
+  setnames(gtf, paste0("V", 1:9))
+  gtf <- gtf[grep("UTR|exon|codon|CDS", V3),]
+  gtf[,"transcript":= sub(paste0(".*", transcript.tag,  "([^;]*).*"), "\\1", V9)]
+  gtf[,"gene":= sub(paste0(".*", gene.tag, "([^:;]*).*"), "\\1", V9)]
+  gtf[,'tags':=paste0('gene_id ', paste0('"', gene, '"'), "; ", 'transcript_id ', 
+                              paste0('"', transcript, '"'), "; ")]
+  gtf <- subset(gtf, T, c(1:8, 12))
+    if(write.to.file){
+    write.table(gtf, file.path, row.names = F, col.names = F, sep='\t', qmethod = "escape", quote = F)  
+    }else{
+      print("hope you know how to write that to a table without messing it")
+      return(gtf)
+    }
+  
+}

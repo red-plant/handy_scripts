f6c1f9e2eac6ffafb43bd74f04ebc91b52c39575
José Alaniz <jaime.alaniz.fabian@gmail.com>
2020-05-21 14:20:57 -0500

    Add files via upload

--- solid.csfa.and.qual.to.csfastq.v2.rb
+++ solid.csfa.and.qual.to.csfastq.v2.rb
@@ -7,13 +7,13 @@ until csFaFile.eof
   line1 = csFaFile.gets.tr(">", "@")
   line2 = csFaFile.gets
   line3 = qualFile.gets.tr(">", "+")
-  phredsAsList=qualFile.gets.split(" ")
-  phred33=phredsAsList.map{ |phredsAsList| (phredsAsList.to_i+33).chr}
-  line4=phred33.join
-  csFqFile.puts "%s" % line1
-  csFqFile.puts "%s" % line2
-  csFqFile.puts "%s" % line3
-  csFqFile.puts "%s" % line4
+  phredsAsList = qualFile.gets.split(" ")
+  phred33 = phredsAsList.map{ |phredsAsList| (phredsAsList.to_i + 33).chr }
+  line4 = phred33.join
+  csFqFile.puts line1
+  csFqFile.puts line2
+  csFqFile.puts line3
+  csFqFile.puts line4
 end
 
 csFaFile.close

e7361409c3c44c2fd16ebbb85b2a407460658a75
José Alaniz <jaime.alaniz.fabian@gmail.com>
2023-06-05 12:49:16 -0600

    Update append.umi.to.read.name.rb

--- append.umi.to.read.name.rb
+++ append.umi.to.read.name.rb
@@ -3,7 +3,24 @@
 #This code takes 3 input arguments (3 fastq files) one with mate1, one with mate2 and one with an UMI.
 # And 2 output arguments: tagged mate 1 and tagged mate2 fastq files. This is for when you have them
 # in different fastq files and need to append the UMI to the read name in their mate 1 and mate 2 
-# files to use with standard de-duplication tools
+# files to use with standard de-duplication tools.
+
+#To run this in multiple files do something like the following (assuming you have them named 
+# .*_R1_.* for mate1, .*_R2_.* for UMI, and .*_R3_.* for mate2).
+
+#for sampl in $sampls; do
+
+#  mateOneTag="${sampl}_R1_001.tagged.fastq"
+#  mateTwoTag="${sampl}_R3_001.tagged.fastq"
+#  mateOne="${sampl}_R1_001.fastq"
+#  mateTwo="${sampl}_R3_001.fastq"
+#  UMI="${sampl}_R2_001.fastq"
+#  pigz -d -p 4 -b 500000 ${mateOne}.gz ${mateTwo}.gz ${UMI}.gz
+#  eval "~/Documents/embryo.in.seed.project/scripts/append.umi.to.read.name.rb $mateOne $mateTwo $UMI $mateOneTag $mateTwoTag"
+#  rm $mateOne $mateTwo $UMI
+#  pigz -p 4 -b 500000 $mateOneTag $mateTwoTag
+
+#done
 
 mateOneFq = File.open(ARGV[0])
 mateTwoFq = File.open(ARGV[1])
